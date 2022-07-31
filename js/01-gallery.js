2c226fa71004de3c0d893ce1f23ccfd30c79d7fc
atamaiNia <bimatideka@gmail.com>
2022-07-31 18:09:16 +0300

    completed task-1

---
+++ js/01-gallery.js
@@ -0,0 +1,84 @@
+// Завдання 1 - галерея зображень
+// Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні.Подивися демо відео роботи галереї.
+//     Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js. Розбий його на декілька підзавдань:
+
+// 1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// 2. Реалізація делегування на div.gallery і отримання url великого зображення.
+// 3. Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// 4. Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// 5. Заміна значення атрибута src елемента < img > в модальному вікні перед відкриттям.
+
+// Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+
+// Розмітка елемента галереї
+// Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+// <div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src="small-image.jpg"
+//       data-source="large-image.jpg"
+//       alt="Image description"
+//     />
+//   </a>
+// </div>
+// Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку.Заборони цю поведінку за замовчуванням.
+
+// Закриття з клавіатури
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотекаи basicLightbox містить метод для програмного закриття модального вікна.
+
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+// створити змінну у якій буде зберігатися уся розмітка галереї
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+// додати розмітку в HTML
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+
+  // const instance і onClose взяті з бібліотеки
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}">`,
+    {
+      onClose: (instance) => {
+        removeEventListener("keydown", onClickEsc);
+      }
+    }
+  );
+
+  function onClickEsc(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+
+  instance.show();
+  return instance;
+}
